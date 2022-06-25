6634f0a343f294d0ebaa603fcfb3f95e5c61b9e6
Sven Peters <Sven.Peters@stud.hs-esslingen.de>
2022-06-25 16:47:58 +0200

    removed Spring application, directory restructuring

--- SpringTest/demo/src/main/java/hse/demo/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package hse.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ItemRepository extends CrudRepository<Item, String> {
-    void deleteById(String name);
-}
